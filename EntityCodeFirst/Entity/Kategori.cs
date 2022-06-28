7f89cd53f475ba7263cfe77ebc13d6e293de5216
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-28 20:21:29 +0300

    Duzenleme yapildi.

--- EntityCodeFirst/Entity/Kategori.cs
+++ EntityCodeFirst/Entity/Kategori.cs
@@ -12,7 +12,7 @@ namespace EntityCodeFirst.Entity
         [Key]
         public int KategoriID { get; set; } 
         public string KategoriDetay { get; set; }  
-        public string Kategoriİsim { get; set; }
+        public string KategoriAdi { get; set; }
 
 
         //Kategorilerin içerisinde birden çok ürün yer alabilir.
