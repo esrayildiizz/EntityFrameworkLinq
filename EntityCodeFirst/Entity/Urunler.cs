79bf3521c689703c6885cc60b1bbdbe04d8cd86c
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-28 14:25:38 +0300

    Entity klasor icine siniflarimizi olusturduk.

---
+++ EntityCodeFirst/Entity/Urunler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCodeFirst.Entity
+{
+    public class Urunler
+    {
+        public int UrunID { get; set; } 
+        public string UrunAd { get; set; }  
+        public string UrunMarka { get; set; }
+        public string UrunKategori { get; set; }    
+        public int UrunStok { get; set; }   
+
+    }
+}
