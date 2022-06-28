7a48372607c66b8324115dc9f4e5834d3a71e862
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-28 16:17:10 +0300

    Migrations dosyasi eklendi. Ve update-database ile veri tabanina yansitildi.

---
+++ EntityCodeFirst/Migrations/202206281254048_AddMigration.cs
@@ -0,0 +1,26 @@
+﻿namespace EntityCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMigration : DbMigration 
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Kategoris",
+                c => new
+                    {
+                        KategoriID = c.Int(nullable: false, identity: true),
+                        KategoriAd = c.String(),
+                    })
+                .PrimaryKey(t => t.KategoriID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Kategoris");
+        }
+    }
+}
