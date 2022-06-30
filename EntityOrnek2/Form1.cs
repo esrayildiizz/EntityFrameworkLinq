f0fb9df58f4e73a350917ee207dc536f401bae13
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-30 13:58:53 +0300

    Min deger bulma kullanildi.

--- EntityOrnek2/Form1.cs
+++ EntityOrnek2/Form1.cs
@@ -27,9 +27,9 @@ namespace EntityOrnek2
 
 
             //Max|Min
-            label1.Text = db.TBLNOTLAR.Max(x => x.ORTALAMA).ToString(); 
-            
+            // label1.Text = db.TBLNOTLAR.Max(x => x.ORTALAMA).ToString(); 
 
+            label1.Text = db.TBLNOTLAR.Min(x => x.SINAV1).ToString();
         }
 
 
